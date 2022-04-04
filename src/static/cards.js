e52f1788c1bdc33f843aa9543c8c51c89bb55834
thomas.kelly <thomas.kelly@signifyd.com>
2022-04-04 21:22:49 +0100

    add logo and smaller res images

--- src/static/cards.js
+++ src/static/cards.js
@@ -5,7 +5,7 @@ const cards = [
     title: "Instagram",
     description:
       "Be sure to check out our Instagram page for lots more great content!",
-    imageUrl: process.env.PUBLIC_URL + "/assets/img1.jpg",
+    imageUrl: process.env.PUBLIC_URL + "/assets/instagram.jpg",
     direction: "left",
     timeout: 1500,
     link: "https://www.instagram.com/ratemyexhibition/",
@@ -14,7 +14,7 @@ const cards = [
     title: "Donate",
     description:
       "If you like what we do and would like to support our work, donations are welcomed on our Patreon.",
-    imageUrl: process.env.PUBLIC_URL + "/assets/img3.jpg",
+    imageUrl: process.env.PUBLIC_URL + "/assets/donate.jpg",
     direction: "right",
     timeout: 1500,
     link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
