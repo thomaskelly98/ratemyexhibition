9b7e4f90509c3e52f91770f38f0c4a531d02e720
Thomas Kelly <thomask540@gmail.com>
2022-04-04 19:42:21 +0100

    updates to layout and animation. Add click response

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,6 +2,8 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import ImageCard from "./ImageCard";
 import cards from "../static/cards";
+import { Grid } from "@material-ui/core";
+import useWindowPosition from "../hook/useWindowPosition";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,15 +11,22 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+    },
+  },
+  grid: {
+    justifyContent: "center",
   },
 }));
 
 export default function Content() {
   const classes = useStyles();
+  const checked = useWindowPosition("header");
   return (
-    <div className={classes.root}>
-      <ImageCard cardInfo={cards[0]} />
-      <ImageCard cardInfo={cards[1]} />
+    <div className={classes.root} id="content">
+      <ImageCard cardInfo={cards[0]} checked={checked} />
+      <ImageCard cardInfo={cards[1]} checked={checked} />
     </div>
   );
 }
--- src/static/cards.js
+++ src/static/cards.js
@@ -7,7 +7,7 @@ const cards = [
       "Be sure to check out our Instagram page for lots more great content!",
     imageUrl: process.env.PUBLIC_URL + "/assets/img1.jpg",
     direction: "left",
-    time: 1500,
+    timeout: 1500,
     link: "https://www.instagram.com/ratemyexhibition/",
   },
   {
@@ -16,7 +16,7 @@ const cards = [
       "If you like what we do and would like to support our work, donations are welcomed on our Patreon.",
     imageUrl: process.env.PUBLIC_URL + "/assets/img3.jpg",
     direction: "right",
-    time: 1500,
+    timeout: 1500,
     link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
   },
 ];
