3c35232b2e1ae2664c1dbd69417cd8678cbdcb32
thomaskelly98 <thomask540@gmail.com>
2022-04-05 14:00:57 +0100

    small updates

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Collapse } from '@material-ui/core';
+import { Collapse, Grid } from '@material-ui/core';
 import ImageCard from './ImageCard';
 import cards from '../static/cards';
 import useWindowPosition from '../hook/useWindowPosition';
@@ -17,7 +17,13 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   grid: {
+    minHeight: '100vh',
+    display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+    },
   },
 }));
 
@@ -27,8 +33,14 @@ export default function Content() {
   return (
     <div className={classes.root} id="content">
       <Collapse in={checked} {...(checked ? { timeout: 1500 } : {})}>
-        <ImageCard cardInfo={cards[0]} checked={checked} />
-        <ImageCard cardInfo={cards[1]} checked={checked} />
+        <Grid
+          container
+          className={classes.grid}
+          style={{ textAlign: 'center' }}
+        >
+          <ImageCard cardInfo={cards[0]} checked={checked} />
+          <ImageCard cardInfo={cards[1]} checked={checked} />
+        </Grid>
       </Collapse>
     </div>
   );
